74e6c58aed491aa36e3908d4fabbc7d884e30d4d
unknown <praveen890340@gmail.com>
2025-02-12 02:26:45 +0530

    first

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: [
+    "./index.html", // ✅ Include index.html in the root folder
+    "./src/**/*.{html,js,jsx,ts,tsx}", // ✅ Scan relevant files inside src/
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        baloo: ["Baloo Bhaijaan 2"],
+      },
+      colors:{
+        "primary":"#881415",
+        "secondary":"#FDF852",
+        "accent":"#282A36",
+        "dark":"#000000",
+        "light":"#FAF3DD",
+        "white":"#ffffff"
+      },
+      fontSize:{
+        "display":"64px"
+      },
+      borderRadius:{
+        "static-card":"12px"
+      }
+    },
+  },
+  plugins: [],
+}
